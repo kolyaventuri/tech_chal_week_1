289809df512119442057a16e3ca4fee5f6adfa06
Kolya Venturi <kolya.venturi@enx3s.com>
2018-06-29 10:14:00 -0600

    Fix tests and add recursion version

--- test/problem_1_test.js
+++ test/problem_1_test.js
@@ -4,22 +4,19 @@ const P1 = require('../lib/problem1');
 
 // Write three functions that compute the sum of the numbers in a given list using a for-loop, a while-loop, and recursion.
 
-const nums = [1,2,3,4,5];
-const expected = 15;
-
 describe('Problem 1', () => {
   it('should be able to sum numbers with a for-loop', () => {
-    let result = P1.for_loop(nums);
-    expect(result).to.eq(expected);
+    let result = P1.for_loop([1,2,3,4,5]);
+    expect(result).to.eq(15);
   });
 
   it('should be able to sum numbers with a while-loop', () => {
-    let result = P1.while_loop(nums);
-    expect(result).to.eq(expected);
+    let result = P1.while_loop([1,2,3,4,5]);
+    expect(result).to.eq(15);
   });
 
-  it('should be able to sum numbers with a for-loop', () => {
-    let result = P1.recursion(nums);
-    expect(result).to.eq(expected);
+  it('should be able to sum numbers with recursion', () => {
+    let result = P1.recursion([1,2,3,4,5]);
+    expect(result).to.eq(15);
   });
 });
