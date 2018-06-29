a218aeddad9b899805a06357e51d8027d0e09c83
Kolya Venturi <kolya.venturi@enx3s.com>
2018-06-29 10:08:08 -0600

    Problem 1 tests

---
+++ test/problem_1_test.js
@@ -0,0 +1,25 @@
+require('./test_helper');
+
+const P1 = require('../lib/problem1');
+
+// Write three functions that compute the sum of the numbers in a given list using a for-loop, a while-loop, and recursion.
+
+const nums = [1,2,3,4,5];
+const expected = 10;
+
+describe('Problem 1', () => {
+  it('should be able to sum numbers with a for-loop', () => {
+    let result = P1.for_loop(nums);
+    expect(result).to.eq(expected);
+  });
+
+  it('should be able to sum numbers with a while-loop', () => {
+    let result = P1.while_loop(nums);
+    expect(result).to.eq(expected);
+  });
+
+  it('should be able to sum numbers with a for-loop', () => {
+    let result = P1.recursion(nums);
+    expect(result).to.eq(expected);
+  });
+});
