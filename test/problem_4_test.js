6acc6d47d0e321f01293a48b9fcea5be0f52f642
Kolya Venturi <kolya.venturi@enx3s.com>
2018-06-29 10:38:50 -0600

    Problem 4 tests

---
+++ test/problem_4_test.js
@@ -0,0 +1,16 @@
+require('./test_helper')
+// Write a function that given a list of non negative integers, arranges them such that they form the largest possible number. For example, given [50, 2, 1, 9], the largest formed number is 95021.
+
+const largest = require('../lib/problem_4');
+
+describe('Problem 4', () => {
+  it('should be able to arrange the largest number', () => {
+    expect([50, 2, 1, 9]).to.eq(95021);
+  });
+
+  it('should be able to do this with different numbers', () => {
+    expect(largest([1,1])).to.eq(11);
+    expect(largest([1,5])).to.eq(51);
+    expect(largest([10,9])).to.eq(910);
+  });
+});
